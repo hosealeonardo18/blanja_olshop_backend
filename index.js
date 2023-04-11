c501929301fd65ca3579df3816f1fc037efee6fc
Hosealeonardo18 <hosealeonardo18@gmail.com>
2023-04-11 21:23:32 +0700

    fix cors

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.all('*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST,GET,PUT, DELETE');
 
     const messageError = err.message || "internal server error"
     const statusCode = err.status || 500
